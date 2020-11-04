7ef7756897566a9064f3d9dd16d2f6be89b42f7a
yury <edenstack17@gmail.com>
2020-11-04 23:20:13 +0400

    Добавлен bean MapperFactory в класс PracticeApplication

--- src/main/java/ru/bellintegrator/practice/PracticeApplication.java
+++ src/main/java/ru/bellintegrator/practice/PracticeApplication.java
@@ -1,13 +1,21 @@
 package ru.bellintegrator.practice;
 
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.impl.DefaultMapperFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
+	}
+
+	@Bean
+	public MapperFactory mapperFactory() {
+		return new DefaultMapperFactory.Builder().build();
 	}
 
 }
