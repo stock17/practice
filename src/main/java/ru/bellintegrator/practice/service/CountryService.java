eb093404cf185e6e5a8a57641e0824869e906350
yury <edenstack17@gmail.com>
2020-11-04 19:34:03 +0400

    Создан интерфейс сервиса для класса Country

--- src/main/java/ru/bellintegrator/practice/service/CountryService.java
+++ src/main/java/ru/bellintegrator/practice/service/CountryService.java
@@ -1,5 +1,36 @@
 package ru.bellintegrator.practice.service;
 
+
+import ru.bellintegrator.practice.model.Country;
+import ru.bellintegrator.practice.view.CountryView;
+
+import java.util.List;
+
+/**
+ * Интерфейс сервиса для работы с классом {@link ru.bellintegrator.practice.model.Country}
+ */
 public interface CountryService {
+    /**
+     * Метод возвращает объект {@link ru.bellintegrator.practice.view.CountryView}
+     * соответствующий переданному коду
+     *
+     * @param code код страны
+     * @return DTO для класса {@link ru.bellintegrator.practice.model.Country}
+     */
+    CountryView findByCode(Integer code);
+
+    /**
+     * Метод сохраняет переданный объект
+     * @param countryView DTO-объект
+     */
+    void addCountry(CountryView countryView);
+
+    /**
+     * Метод возращает все найденные страны в виде списка DTO-объектов
+     * @return список {@link ru.bellintegrator.practice.model.Country}
+     */
+    List<CountryView> findAll();
+
+
 
 }
