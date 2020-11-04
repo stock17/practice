1d74dcd106cbb67815bcb0b355a83a57eeb26c4d
yury <edenstack17@gmail.com>
2020-11-05 02:03:52 +0400

    Добавлен DTO-класс DocumentTypeView

---
+++ src/main/java/ru/bellintegrator/practice/view/DocumentTypeView.java
@@ -0,0 +1,39 @@
+package ru.bellintegrator.practice.view;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * DTO-класс для {@link ru.bellintegrator.practice.model.DocumentType}
+ */
+public class DocumentTypeView {
+    /**
+     * Числовой код документа
+     */
+    @NotNull(message = "Код не может быть пустым")
+    private Integer code;
+
+    /**
+     * Наименование документа
+     */
+    @Size(max = 157)
+    @NotEmpty(message = "Наименование не может быть пустым")
+    private String name;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
