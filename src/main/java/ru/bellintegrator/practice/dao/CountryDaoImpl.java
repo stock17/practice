72a537b19c6bf542fc8ae0151a780942aae4ac24
yury <edenstack17@gmail.com>
2020-11-04 17:44:47 +0400

    Создана реализация DAO-интерфейса для класса Country

---
+++ src/main/java/ru/bellintegrator/practice/dao/CountryDaoImpl.java
@@ -0,0 +1,44 @@
+package ru.bellintegrator.practice.dao;
+
+import ru.bellintegrator.practice.daointerface.CountryDao;
+import ru.bellintegrator.practice.model.Country;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+/**
+ * Реализация интерфейса {@link ru.bellintegrator.practice.daointerface.CountryDao}
+ * для работы с базой данных по спецификации JPA
+ */
+
+public class CountryDaoImpl implements CountryDao {
+
+    @PersistenceContext
+    EntityManager em;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Country findByCode(Integer code) {
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<Country> criteria = builder.createQuery(Country.class);
+        Root<Country> root = criteria.from(Country.class);
+        criteria.where(builder.equal(root.get("code"), code));
+        return em.createQuery(criteria).getSingleResult();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+
+    @Override
+    public List<Country> findAll() {
+        CriteriaQuery<Country> criteria = em.getCriteriaBuilder().createQuery(Country.class);
+        return em.createQuery(criteria).getResultList();
+    }
+
+}
