ca75b17927b4c0e4aba3f08f2d2ee5154bd4a82e
yury <edenstack17@gmail.com>
2020-11-05 14:50:00 +0400

    Добавлен интерфейс OrganizationService и его реализация

---
+++ src/main/java/ru/bellintegrator/practice/service/OrganizationServiceImpl.java
@@ -0,0 +1,64 @@
+package ru.bellintegrator.practice.service;
+
+import ma.glasnost.orika.MapperFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.bellintegrator.practice.daointerface.OrganizationDao;
+import ru.bellintegrator.practice.model.Organization;
+import ru.bellintegrator.practice.serviceinterface.OrganizationService;
+import ru.bellintegrator.practice.view.OrganizationView;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+/**
+ * Реализация интерфейса {@link ru.bellintegrator.practice.serviceinterface.OrganizationService}
+ * с использованием библиотеки {@link ma.glasnost.orika}
+ */
+@Service
+public class OrganizationServiceImpl implements OrganizationService {
+
+    private final OrganizationDao dao;
+    private final MapperFactory mapperFactory;
+
+    @Autowired
+    public OrganizationServiceImpl(OrganizationDao dao, MapperFactory mapperFactory) {
+        this.dao = dao;
+        this.mapperFactory = mapperFactory;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public OrganizationView findById(long id) {
+        Organization organization = dao.findById(id);
+        return mapperFactory.getMapperFacade().map(organization, OrganizationView.class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addOrganization(OrganizationView organizationView) {
+        Organization organization = mapperFactory.getMapperFacade().map(organizationView, Organization.class);
+        dao.add(organization);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<OrganizationView> findAll() {
+        List<Organization> organizations = dao.findAll();
+        return mapperFactory.getMapperFacade().mapAsList(organizations, OrganizationView.class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<OrganizationView> findByName(@NotEmpty String name, String inn, Boolean isActive) {
+        List<Organization> organizations = dao.findByName(name, inn, isActive);
+        return mapperFactory.getMapperFacade().mapAsList(organizations, OrganizationView.class);
+    }
+}
---
+++ src/main/java/ru/bellintegrator/practice/serviceinterface/OrganizationService.java
@@ -0,0 +1,45 @@
+package ru.bellintegrator.practice.serviceinterface;
+
+
+import ru.bellintegrator.practice.view.DocumentTypeView;
+import ru.bellintegrator.practice.view.OrganizationView;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+/**
+ * Интерфейс сервиса для работы с классом {@link ru.bellintegrator.practice.model.Organization}
+ */
+public interface OrganizationService {
+    /**
+     * Метод возвращает объект {@link ru.bellintegrator.practice.view.OrganizationView}
+     * соответствующий переданному Id
+     *
+     * @param id идентификатор организации
+     * @return DTO для класса {@link ru.bellintegrator.practice.model.Organization}
+     */
+    OrganizationView findById(long id);
+
+    /**
+     * Метод сохраняет переданный объект
+     * @param organizationView DTO-объект
+     */
+    void addOrganization(OrganizationView organizationView);
+
+    /**
+     * Метод возращает все найденые организации в виде списка DTO-объектов
+     * @return список {@link ru.bellintegrator.practice.view.OrganizationView}
+     */
+    List<OrganizationView> findAll();
+
+
+    /**
+     * Метод возращает все найденые организации в виде списка DTO-объектов
+     *
+     * @param name наименование организации (обязательный параметр)
+     * @param inn идентификационный налоговый номер
+     * @param isActive действующий статус
+     * @return список {@link ru.bellintegrator.practice.view.OrganizationView}
+     */
+    List<OrganizationView> findByName(@NotEmpty String name, String inn, Boolean isActive);
+}
