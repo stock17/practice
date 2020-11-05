68cf0719a5d849256edb1e509067dab43390a0c2
yury <edenstack17@gmail.com>
2020-11-05 17:51:59 +0400

    Добавлен DTO-класс OrganizationShortView

---
+++ src/main/java/ru/bellintegrator/practice/view/OrganizationShortView.java
@@ -0,0 +1,55 @@
+package ru.bellintegrator.practice.view;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+/**
+ * DTO-класс для {@link ru.bellintegrator.practice.model.Organization}
+ * для передачи параметров в кратком виде
+ */
+public class OrganizationShortView {
+
+    /**
+     * Id
+     */
+    private long id;
+
+    /**
+     * Краткое наименование организации
+     */
+    @NotEmpty(message = "Наименование не может быть пустым")
+    @Size(max = 50)
+    private String name;
+
+    /**
+     * Действующий статус
+     */
+    @NotNull(message = "Статус не может быть не установленным")
+    private Boolean isActive;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+}
--- src/main/java/ru/bellintegrator/practice/view/OrganizationView.java
+++ src/main/java/ru/bellintegrator/practice/view/OrganizationView.java
@@ -5,6 +5,9 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+/**
+ * DTO-класс для {@link ru.bellintegrator.practice.model.Organization}
+ */
 public class OrganizationView {
     /**
      * Id
