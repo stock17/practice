6f5162f6d8dd280e828b6bb852a08182a2f2ede0
yury <edenstack17@gmail.com>
2020-11-11 19:10:07 +0400

    Добавлены сеттеры и геттеры в класс Document

--- src/main/java/ru/bellintegrator/practice/model/Document.java
+++ src/main/java/ru/bellintegrator/practice/model/Document.java
@@ -33,4 +33,52 @@ public class Document {
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     @MapsId("id")
     private User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public Integer getDocNumber() {
+        return docNumber;
+    }
+
+    public void setDocNumber(Integer docNumber) {
+        this.docNumber = docNumber;
+    }
+
+    public Date getDocDate() {
+        return docDate;
+    }
+
+    public void setDocDate(Date docDate) {
+        this.docDate = docDate;
+    }
+
+    public Integer getDocCode() {
+        return docCode;
+    }
+
+    public void setDocCode(Integer docCode) {
+        this.docCode = docCode;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
