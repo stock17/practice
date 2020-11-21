d2f21d7f4502cd0a327ff42cca2e30d93c5a0e70
yury <edenstack17@gmail.com>
2020-11-21 14:51:00 +0400

    Добавлен DAO интерфейс DocumentDao и его реализация

---
+++ src/main/java/ru/bellintegrator/practice/daointerface/DocumentDao.java
@@ -0,0 +1,29 @@
+package ru.bellintegrator.practice.daointerface;
+
+import ru.bellintegrator.practice.model.Document;
+
+/**
+ * DAO интерфейс для класса {@link ru.bellintegrator.practice.model.Document}
+ */
+public interface DocumentDao {
+
+    /**
+     * Метод добавляет новый документ {@link ru.bellintegrator.practice.model.Document}
+     * @param document документ
+     */
+    void save(Document document);
+
+    /**
+     * Метод возвращает {@link ru.bellintegrator.practice.model.Document} по Id
+     *
+     * @param id идентификатор
+     * @return объект {@link ru.bellintegrator.practice.model.Document}
+     */
+    Document findById(long id);
+
+    /**
+     * Метод обноваляет данные уже существующего документа {@link ru.bellintegrator.practice.model.Document}
+     * @param document документа
+     */
+    void update(Document document);
+}
