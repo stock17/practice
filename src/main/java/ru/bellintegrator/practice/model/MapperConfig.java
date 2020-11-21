e16363dc30f21010b7abb1d4158e50a536de70d1
yury <edenstack17@gmail.com>
2020-11-21 12:40:31 +0400

    Конфигурация Mapper вынесена в отдельный файл

---
+++ src/main/java/ru/bellintegrator/practice/model/MapperConfig.java
@@ -0,0 +1,45 @@
+package ru.bellintegrator.practice.model;
+
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.impl.DefaultMapperFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.bellintegrator.practice.view.user.UserListRequestView;
+import ru.bellintegrator.practice.view.user.UserUpdateView;
+import ru.bellintegrator.practice.view.user.UserView;
+
+@Configuration
+public class MapperConfig {
+    @Bean
+    public MapperFactory mapperFactory() {
+
+        DefaultMapperFactory.Builder builder = new DefaultMapperFactory.Builder();
+        MapperFactory factory = builder.mapNulls(false).build();
+
+        factory.classMap(User.class, UserView.class)
+                .field("citizenship.name", "citizenshipName")
+                .field("citizenship.code", "citizenshipCode")
+                .byDefault()
+                .register();
+
+        factory.classMap(Document.class, UserView.class)
+                .field("documentType.code", "docCode")
+                .field("documentType.name", "docName")
+                .byDefault()
+                .register();
+
+        factory.classMap(UserListRequestView.class, User.class)
+                .field("officeId", "office.id")
+                .field("citizenshipCode", "citizenship.code")
+                .byDefault()
+                .register();
+
+        factory.classMap(UserUpdateView.class, User.class)
+                .field("officeId", "office.id")
+                .field("citizenshipCode", "citizenship.code")
+                .byDefault()
+                .register();
+
+        return factory;
+    }
+}
