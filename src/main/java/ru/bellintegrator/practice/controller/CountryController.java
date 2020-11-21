f89214d196d8bbf3891f8f8aa9737f14246a6ebe
yury <edenstack17@gmail.com>
2020-11-21 14:52:25 +0400

    В CountryController добавлен метод getByCode

--- src/main/java/ru/bellintegrator/practice/controller/CountryController.java
+++ src/main/java/ru/bellintegrator/practice/controller/CountryController.java
@@ -2,6 +2,7 @@ package ru.bellintegrator.practice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.bellintegrator.practice.serviceinterface.CountryService;
@@ -33,5 +34,10 @@ public class CountryController {
     @GetMapping("/")
     public List<CountryView> getCountries() {
         return service.findAll();
+    }
+
+    @GetMapping("/{code}")
+    public CountryView getByCode(@PathVariable Integer code) {
+        return service.findByCode(code);
     }
 }
